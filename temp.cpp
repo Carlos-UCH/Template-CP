bd450dbed8d3d82792df042d68ef6999e475154c
Carlos-UCH <carlosbrasil254@gmail.com>
2025-07-16 23:14:30 -0300

    add: template

---
+++ temp.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+using namespace std; 
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define vpll vector<pair<ll, ll>>
+#define endl '\n'
+#define found(x, y) (x.find(y) != x.end()) //set - map - unordered_set - unordered_map
+#define debug(x) cout << #x << " = " << x << endl
+
+const int inf = 0x3f3f3f3f;
+const ll linf = 0x3f3f3f3f3f3f3f3fll;
+ll meuJeitoNinja = 0; 
+
+template<typename Container, typename T>
+bool fd(const T& value, const Container& container) {
+    return find(container.begin(), container.end(), value) != container.end();
+} 
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+	
+	
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
