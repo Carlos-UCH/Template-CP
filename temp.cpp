ab858d2a1d7e4354b450643f9e54480a852d9729
Carlos-UCH <carlosbrasil254@gmail.com>
2025-07-16 23:19:08 -0300

    update: template

--- temp.cpp
+++ temp.cpp
@@ -7,15 +7,19 @@ using namespace std;
 #define vi vector<int>
 #define pb push_back
 #define mp make_pair
-#define ff first
-#define ss second
+#define f first
+#define s second
 #define all(x) x.begin(), x.end()
 #define vll vector<ll>
 #define pll pair<ll, ll>
 #define vpll vector<pair<ll, ll>>
 #define endl '\n'
 #define found(x, y) (x.find(y) != x.end()) //set - map - unordered_set - unordered_map
-#define debug(x) cout << #x << " = " << x << endl
+#define sz(x) (int)x.size()
+
+//loops
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define repinv(i,a,b) for(int i = (a); i >= (b); i--)
 
 const int inf = 0x3f3f3f3f;
 const ll linf = 0x3f3f3f3f3f3f3f3fll;
@@ -23,16 +27,16 @@ ll meuJeitoNinja = 0;
 
 template<typename Container, typename T>
 bool fd(const T& value, const Container& container) {
-    return find(container.begin(), container.end(), value) != container.end();
+  return find(container.begin(), container.end(), value) != container.end();
 } 
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); 
+  ios_base::sync_with_stdio(0);
+  cin.tie(0); 
 	
 	
 
-    return 0;
+  return 0;
 
 }
 
