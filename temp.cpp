f0b7b067bfc1a9ed472de39af3e80321be93c685
Carlos-UCH <carlosbrasil254@gmail.com>
2025-11-01 18:33:30 -0300

    fix:  template define

--- temp.cpp
+++ temp.cpp
@@ -21,7 +21,11 @@ using namespace std;
 #define vvpll vector<vpll>
 #define endl '\n'
 #define found(x, y) (x.find(y) != x.end()) //set - map - unordered_set - unordered_map
-#define debug(x) cout << #x << " = " << x << endl
+#define sz(x) (int)x.size()
+
+//loops
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define repinv(i,a,b) for(int i = (a); i >= (b); i--)
 
 const int inf = 0x3f3f3f3f;
 const ll linf = 0x3f3f3f3f3f3f3f3fll;
@@ -29,20 +33,17 @@ ll meuJeitoNinja = 0;
 
 template<typename Container, typename T>
 bool fd(const T& value, const Container& container) {
-    return find(container.begin(), container.end(), value) != container.end();
+  return find(container.begin(), container.end(), value) != container.end();
 } 
 
 signed main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0); 
-	
-	
+  
+
   return 0;
 
 }
-
-
-
 
 
 
