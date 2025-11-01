477894ddc08ea2050cbcb93632b51351c2e61e62
Carlos-UCH <carlosbrasil254@gmail.com>
2025-11-01 18:22:32 -0300

    add debug

--- temp.cpp
+++ temp.cpp
@@ -2,11 +2,15 @@
 
 using namespace std; 
 
+#ifdef LOCAL
+#include "dbg/dbg.h"
+#else
+#define dbg(...)
+#endif
 #define ll long long
 #define ld long double
 #define vi vector<int>
 #define pb push_back
-#define eb emplace_back
 #define mp make_pair
 #define ff first
 #define ss second
@@ -17,11 +21,7 @@ using namespace std;
 #define vvpll vector<vpll>
 #define endl '\n'
 #define found(x, y) (x.find(y) != x.end()) //set - map - unordered_set - unordered_map
-#define sz(x) (int)x.size()
-
-//loops
-#define rep(i, a, b) for(int i = (a); i < (b); i++)
-#define repinv(i,a,b) for(int i = (a); i >= (b); i--)
+#define debug(x) cout << #x << " = " << x << endl
 
 const int inf = 0x3f3f3f3f;
 const ll linf = 0x3f3f3f3f3f3f3f3fll;
@@ -29,15 +29,14 @@ ll meuJeitoNinja = 0;
 
 template<typename Container, typename T>
 bool fd(const T& value, const Container& container) {
-  return find(container.begin(), container.end(), value) != container.end();
+    return find(container.begin(), container.end(), value) != container.end();
 } 
 
-int main() {
+signed main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0); 
 	
 	
-
   return 0;
 
 }
